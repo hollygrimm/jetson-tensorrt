f0f803435eea3182d4b953d65a25c2905b98d8ea
Holly Grimm <holly.grimm@gmail.com>
2019-05-08 10:39:14 -0600

    format

--- my-recognition.cpp
+++ my-recognition.cpp
@@ -1,31 +1,31 @@
 #include <jetson-inference/imageNet.h>
 #include <jetson-utils/loadImage.h>
 
-int main( int argc, char** argv )
+int main(int argc, char **argv)
 {
-	if( argc < 2 )
+	if (argc < 2)
 	{
 		printf("my-recognition: expected image filename as argument\n");
 		printf("example usage: ./my-recognition my_image.jpg\n");
 		return 0;
 	}
 
-	const char* imgFilename = argv[1];
+	const char *imgFilename = argv[1];
 
-	float* imgCPU	= NULL;
-	float* imgCUDA	= NULL;
-	int    imgWidth	= 0;
-	int    imgHeight = 0;
+	float *imgCPU = NULL;
+	float *imgCUDA = NULL;
+	int imgWidth = 0;
+	int imgHeight = 0;
 
-	if( !loadImageRGBA(imgFilename, (float4**)&imgCPU, (float4**)&imgCUDA, &imgWidth, &imgHeight) )
+	if (!loadImageRGBA(imgFilename, (float4 **)&imgCPU, (float4 **)&imgCUDA, &imgWidth, &imgHeight))
 	{
 		printf("failed to load image '%s'\n", imgFilename);
 		return 0;
 	}
 
-	imageNet* net = imageNet::Create(imageNet::GOOGLENET);
+	imageNet *net = imageNet::Create(imageNet::GOOGLENET);
 
-	if( !net )
+	if (!net)
 	{
 		printf("failed to load image recognition network\n");
 		return 0;
@@ -40,12 +40,12 @@ int main( int argc, char** argv )
 	// class 0295 - 0.988976  (American black bear, black bear, Ursus americanus, Euarctos americanus)
 	// class 0297 - 0.010161  (sloth bear, Melursus ursinus, Ursus ursinus)
 
-	if( classIndex >= 0 )
+	if (classIndex >= 0)
 	{
-		const char* classDescription = net->GetClassDesc(classIndex);
+		const char *classDescription = net->GetClassDesc(classIndex);
 
-		printf("image is recognized as '%s' (class #%i) with %f%% confidence\n", 
-			  classDescription, classIndex, confidence * 100.0f);
+		printf("image is recognized as '%s' (class #%i) with %f%% confidence\n",
+			   classDescription, classIndex, confidence * 100.0f);
 	}
 	else
 	{
@@ -54,5 +54,4 @@ int main( int argc, char** argv )
 
 	delete net;
 	return 0;
-	
 }
